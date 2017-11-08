5cc2a40c95b770402abd2acc0385f32383467f36
PeterPoelzl <33464174+PeterPoelzl@users.noreply.github.com>
2017-11-08 01:19:22 +0100

    Update Berechnung.java
    "sum" muss ein long sein, ansonsten gibt es einen Overflow beim Testen mit dem MUL Upload Client.

--- UE3/src/Berechnung.java
+++ UE3/src/Berechnung.java
@@ -1,6 +1,6 @@
 public class Berechnung {
     public static long gibSumme(int anfang, int ende){
-        int sum = 0;
+        long sum = 0;
         for (int i = anfang + anfang % 2; i <= ende; i+= 2){
             sum += i;
         }
